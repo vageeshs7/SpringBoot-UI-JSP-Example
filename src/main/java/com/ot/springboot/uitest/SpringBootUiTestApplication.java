9436cf4d287a39ec0f42b5cbf11603aad8aa9e44
vageesh <vageeshs7@gmail.com>
2019-08-17 14:25:40 +0530

    Initial commit

---
+++ src/main/java/com/ot/springboot/uitest/SpringBootUiTestApplication.java
@@ -0,0 +1,13 @@
+package com.ot.springboot.uitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUiTestApplication.class, args);
+	}
+
+}
