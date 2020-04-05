393f172870877072e5c75d7fdcb8350d2ee8489d
vageesh <vageeshs7@gmail.com>
2020-04-05 15:18:57 +0530

    Adding JMS and QPID AMQP dependencies

--- src/main/java/com/ot/springboot/uitest/services/TodoService.java
+++ src/main/java/com/ot/springboot/uitest/services/TodoService.java
@@ -5,6 +5,16 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Queue;
+import org.apache.qpid.jms.JmsConnectionFactory;
 import org.springframework.stereotype.Service;
 
 import com.ot.springboot.uitest.dom.Todo;
@@ -37,8 +47,27 @@ public class TodoService {
             todoCount++;
             todo.setId(todoCount);
         }
-
         todos.add(todo);
+
+        //Send message to back end
+        String jmsHost = "amqps://hk-np1.fs-solace.dev.net:10130";
+        String jmsUsername = "app-test-user";
+        String jmsPassword = "app-test-user";
+        System.out.printf("Publisher is connecting to JMS messaging at %s...%n", jmsHost);
+
+        // Programmatically create the connection factory using default settings
+        ConnectionFactory connectionFactory = new JmsConnectionFactory(solaceUsername, solacePassword, solaceHost);
+
+        // Create connection to the Solace messaging
+        Connection connection = connectionFactory.createConnection();
+
+        // Create a non-transacted, client ACK session.
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+        System.out.printf("Connected with username '%s'.%n", solaceUsername);
+
+        // Create the queue programmatically and the corresponding messaging resource
+        Queue queue = session.createQueue("q-vas-test-conn");
     }
 
     public void markTodo(int id, boolean isDone){
